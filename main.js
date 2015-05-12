584ec6068503f4ba5dd6d4c7ecda3a5bd11c4adb
Kelley Rose <krose422@gmail.com>
2015-05-12 16:24:58 -0400

    Added subtract, multiply and divide buttons

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var answer = document.querySelector("#answer");
-var calculateBtn = document.querySelector("#calculateBtn");
+var addBtn = document.querySelector("#addBtn");
+var subtractBtn = document.querySelector("#subtractBtn");
+var multiplyBtn = document.querySelector("#multiplyBtn");
+var divideBtn = document.querySelector("#divideBtn");
 var numOne = document.querySelector("#num_one");
 var numTwo = document.querySelector("#num_two");
 
@@ -10,4 +13,28 @@ function add(chosenNum1, chosenNum2) {
   return chosenNum1 + chosenNum2;
 }
 
-calculateBtn.addEventListener("click", add);
+function subtract(chosenNum1, chosenNum2) {
+  var chosenNum1 = Number(numOne.value);
+  var chosenNum2 = Number(numTwo.value);
+  answer.innerHTML = chosenNum1 - chosenNum2;
+  return chosenNum1 - chosenNum2;
+}
+
+function multiply(chosenNum1, chosenNum2) {
+  var chosenNum1 = Number(numOne.value);
+  var chosenNum2 = Number(numTwo.value);
+  answer.innerHTML = chosenNum1 * chosenNum2;
+  return chosenNum1 * chosenNum2;
+}
+
+function divide(chosenNum1, chosenNum2) {
+  var chosenNum1 = Number(numOne.value);
+  var chosenNum2 = Number(numTwo.value);
+  answer.innerHTML = chosenNum1 / chosenNum2;
+  return chosenNum1 / chosenNum2;
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
